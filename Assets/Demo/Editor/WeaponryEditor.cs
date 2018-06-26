6fcd6c00f15ab38e19e311d8fccc44e883fe3391
Redouane Sobaihi <s-redouane@outlook.com>
2018-06-26 22:19:13 +0100

    WIP: prepare demo scene assets.

---
+++ Assets/Demo/Editor/WeaponryEditor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using WeaponsInventorySystem;
+
+public class WeaponeryEditor : Editor
+{
+
+    private static Transform selectedTransform = null;
+
+    [MenuItem("FPS Weaponry/Set As Normal Sight")]
+    public static void SetAsDefaultPosition()
+    {
+        if (!(selectedTransform = Selection.activeTransform) && Selection.activeTransform.CompareTag("Game Weapon"))
+            return;
+        selectedTransform.GetComponent<WeaponSight>().NormalPosition = (selectedTransform.localPosition);
+    }
+
+    [MenuItem("FPS Weaponry/Set As Aim Sight")]
+    public static void SetAsIronSightPosition()
+    {
+        if (!(selectedTransform = Selection.activeTransform) && Selection.activeTransform.CompareTag("Game Weapon"))
+            return;
+        selectedTransform.GetComponent<WeaponSight>().AimPosition = (selectedTransform.localPosition);
+    }
+
+    [MenuItem("FPS Weaponry/Set To Normal Sight")]
+    public static void SetToDefaultPosition()
+    {
+        if (!(selectedTransform = Selection.activeTransform) && Selection.activeTransform.CompareTag("Game Weapon"))
+            return;
+        selectedTransform.localPosition = selectedTransform.GetComponent<WeaponSight>().NormalPosition;
+    }
+
+    [MenuItem("FPS Weaponry/Set To Aim Sight")]
+    public static void SetToIronSightPosition()
+    {
+        if (!(selectedTransform = Selection.activeTransform) && Selection.activeTransform.CompareTag("Game Weapon"))
+            return;
+        selectedTransform.localPosition = selectedTransform.GetComponent<WeaponSight>().AimPosition;
+    }
+
+}
---
+++ Assets/Demo/Scripts/InventoryTestScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using WeaponsInventorySystem;
+using WeaponsInventorySystem.Helpers;
+
+public class InventoryTestScript : MonoBehaviour
+{
+
+	[SerializeField]
+	private Inventory inventory;
+
+	private void Awake()
+	{
+		if (inventory == null)
+		{
+			Debug.LogException(new NullReferenceException("Field 'inventory' must be initialized."));
+		}
+		else
+		{
+			inventory.OnBeginItemChange += Inventory_OnBeginItemChange;
+			inventory.OnItemChanged += Inventory_OnItemChanged;
+			inventory.OnEndItemChange += Inventory_OnEndItemChange;
+		}
+	}
+
+	private void Inventory_OnEndItemChange(object sender, InventoryWeaponEventArgs e)
+	{
+		Debug.LogFormat("#Event: Inventory.OnEndItemChange\nCurrent Item name: '{0}'.\nNew Item name: '{1}'.", e.Current.Name, e.New.Name);
+	}
+
+	private void Inventory_OnItemChanged(object sender, InventoryWeaponEventArgs e)
+	{
+		Debug.LogFormat("#Event: Inventory.OnItemChanged\nCurrent Item name: '{0}'.\nNew Item name: '{1}'.", e.Current.Name, e.New.Name);
+	}
+
+	private void Inventory_OnBeginItemChange(object sender, InventoryWeaponEventArgs e)
+	{
+		Debug.LogFormat("#Event: Inventory.OnBeginItemChange\nCurrent Item name: '{0}'.\nNew Item name: '{1}'.", e.Current.Name, e.New.Name);
+	}
+
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+
+	}
+}
